76a7a291d3561cd3b61f95f23071d24a99812769
YUIGGGG <163645924+YUIGGGG@users.noreply.github.com>
2024-05-23 22:30:42 +0800

    enhancing

--- TestTV.py
+++ TestTV.py
@@ -1,24 +1,21 @@
 from TV import TV  # Import the TV class
 
-
 def main():
     tv1 = TV()  # First object
-    tv1.turn_on()  # Turns on tv1
-    tv1.set_channel(37)  # Set the channel to 30.
-    tv1.set_volume(5)  # Set the volume level to 3.
+    tv1.turnOn()  # Turns on tv1
+    tv1.setChannel(30)  # Set the channel to 30
+    tv1.setVolume(3)  # Set the volume level to 3
 
     tv2 = TV()  # Second object
-    tv2.turn_on()  # Turns on tv2
-    tv2.set_channel(38)
-    tv2.set_volume(3)  # Increase volume by 1
+    tv2.turnOn()  # Turns on tv2
+    tv2.setChannel(3)  # Set the channel to 3
+    tv2.setVolume(2)  # Set the volume level to 2
 
-    # Print the channel and volume level of tv1.
-    print("tv1's channel is", tv1.get_channel(),
-          "and volume Level is", tv1.get_volume_level())
+    # Print the channel and volume level of tv1
+    print("tv1's channel is", tv1.getChannel(), "and volume level is", tv1.getVolume())
 
-    # Print the channel and volume level of tv2.
-    print("tv2's channel is", tv2.get_channel(),
-          "and volume level is", tv2.get_volume_level())
+    # Print the channel and volume level of tv2
+    print("tv2's channel is", tv2.getChannel(), "and volume level is", tv2.getVolume())
 
-
-main()  # Call the main function execute the program.
+if __name__ == "__main__":
+    main()  # Call the main function to execute the program
